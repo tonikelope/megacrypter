b799789c775d73bd073f667d616ee8292ca0b26e
tonikelope <tonikelope@gmail.com>
2014-11-17 21:55:44 +0100

    generic password as argument

--- lib/reporter_id_gen.php
+++ lib/reporter_id_gen.php
@@ -2,8 +2,9 @@
 
 <?php
 
-	define('GENERIC_PASSWORD', ''); //Usar misma contraseña de application/config
-
-	echo "\n".base64_encode("{$argv[1]}#".hash_hmac('sha1', $argv[1], base64_decode(GENERIC_PASSWORD)))."\n";
+	if($argc>=3)
+		echo "\n".base64_encode("{$argv[1]}#".hash_hmac('sha1', $argv[1], $argv[2]))."\n";
+	else
+		echo "\n{$argv[0]} reporter_email hmac_key\n\n";
 
 ?>
