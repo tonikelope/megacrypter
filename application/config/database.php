82d8f917b49ca6ea5bbe11baa99de03a76ee14cf
A. L. Vivar <tonikelope@gmail.com>
2014-11-18 10:41:53 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,4 +1,19 @@
 <?php
+    
+    /*
+    
+    DATABASE IS OPTIONAL (just for blacklist link list)
+    
+    DROP TABLE IF EXISTS `blacklist`;
+
+    CREATE TABLE `blacklist` (
+      `id` varchar(512) NOT NULL DEFAULT '',
+      `reporter` varchar(255) DEFAULT NULL,
+      `ip` varchar(15) DEFAULT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+    
+    */
 
     define('DB_TYPE', 'mysql');
     define('DB_HOST', 'localhost');
