acf96ff71ce26dca0c351922189562d7b4f51fcd
A. L. Vivar <tonikelope@gmail.com>
2014-11-22 15:12:38 +0100

    Update miscellaneous.php

--- application/config/miscellaneous.php
+++ application/config/miscellaneous.php
@@ -1,6 +1,6 @@
 <?php
 
-    define('URL_BASE', 'http://megacrypter.yourdomain.com'); //domain or subdomain is required (API URL is relative to root path) 
+    define('URL_BASE', 'http://megacrypter.yourdomain.com'); //Domain or subdomain is required (API URL is relative to root path) 
     define('MASTER_KEY', 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'); //YOU MUST GENERATE YOUR OWN AES KEY (and keep it) 
     define('GENERIC_PASSWORD', 'CgCv8QmhXjbcmdhNFvPuUw=='); //IDEM AS AES KEY (min recommeded 16 bytes base64-encoded)
     define('GOOGLE_URL_SHORT_API_KEY', ''); //Required for tiny urls
