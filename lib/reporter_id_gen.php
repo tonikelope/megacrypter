123119aa0963ed8ea19255f0d36c29ca27064ba7
tonikelope <tonikelope@gmail.com>
2016-06-03 16:33:14 +0200

    fix legacy

--- lib/reporter_id_gen.php
+++ lib/reporter_id_gen.php
@@ -3,7 +3,7 @@
 <?php
 
 	if($argc>=3)
-		echo "\n".base64_encode("{$argv[1]}#".hash_hmac('sha1', $argv[1], base64_decode($argv[2])))."\n";
+		echo "\n".base64_encode("{$argv[1]}#".hash_hmac('sha256', $argv[1], $argv[2]))."\n";
 	else
 		echo "\n{$argv[0]} reporter_email generic_pass\n\n";
 
